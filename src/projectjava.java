12aa47dae988115ed017133a6ab43187fb3a38c8
CGStudent <danielle@teamalyx.com>
2023-03-28 12:31:41 +0200

    my commit

---
+++ src/projectjava.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+import static com.jogamp.newt.opengl.util.NEWTDemoListener.setTitle;
+
+public class projectjava {
+    private JPanel mainPanel;
+    private JTextField insertName;
+    private JLabel timeslot;
+    private JLabel timeslotMin;
+    private JButton addParticipantButton;
+    private JButton button2;
+    private JLabel tfFirstName;
+    private JLabel tfLastName;
+    private JTextField textField3;
+    private JPanel colpanel1;
+    private JPanel mainpanel2;
+    private JPanel mainpanel3;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+
+}
+
+/*public class MainF extends JFame {
+
+    public MainF() {
+        SetContentPane (MainFrame);
+        setTitle("Welcome");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible =(true);
+    }
+
+    private void setTitle(String welcome) {
+    }
+
+    public static void main(String[] args) {
+        MainFrame myFrame = new MainFraime();
+        */
+
+
