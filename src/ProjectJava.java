efa6bd4932e7fe90259fe15fa4606361a994a5d2
CGStudent <danielle@teamalyx.com>
2023-03-30 14:08:41 +0200

    changed char limitation to also included ä, ë, ö, ï, ü and - and spaces.

--- src/ProjectJava.java
+++ src/ProjectJava.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import java.awt.*;
 
 public class ProjectJava extends JFrame {
     private JPanel mainPanel;
@@ -39,9 +44,68 @@ public class ProjectJava extends JFrame {
         SpinnerNumberModel minutesModel = new SpinnerNumberModel(0, 0, 59, 1);
         input_minutes.setModel(minutesModel);
 
-        // Add the rest of your code here
-        // ...
+        clear_btn.addActionListener(e -> {
+            insertFn.setText("");
+            insertLN.setText("");
+            input_hours.setValue(input_hours.getMinimumSize());
+            input_minutes.setValue(input_minutes.getMinimumSize());
+        });
+
+        ((AbstractDocument) insertFn.getDocument()).setDocumentFilter(new DocumentFilter() {
+            private final String regex = "^[a-zA-Z\\s\\-ëäöïü]{0,50}$";
+
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                String currentText = fb.getDocument().getText(0, fb.getDocument().getLength());
+                String beforeText = currentText.substring(0, offset);
+                String afterText = currentText.substring(offset + length, currentText.length());
+                String proposedText = beforeText + text + afterText;
+                if (proposedText.matches(regex)) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+        });
+
+        ((AbstractDocument) insertLN.getDocument()).setDocumentFilter(new DocumentFilter() {
+            private final String regex = "^[a-zA-Z\\s\\-ëäöïü]{0,50}$";
+
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                String currentText = fb.getDocument().getText(0, fb.getDocument().getLength());
+                String beforeText = currentText.substring(0, offset);
+                String afterText = currentText.substring(offset + length, currentText.length());
+                String proposedText = beforeText + text + afterText;
+                if (proposedText.matches(regex)) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+        });
+
+        // Add action listener to addParticipantButton
+        addParticipantButton.addActionListener(e -> {
+            saveParticipantInfo();
+        });
+    }
+
+    private void saveParticipantInfo() {
+        String firstName = insertFn.getText();
+        String lastName = insertLN.getText();
+        int hours = (int) input_hours.getValue();
+        int minutes = (int) input_minutes.getValue();
+
+        // Check if all fields are filled in
+        if (!firstName.isEmpty() && !lastName.isEmpty() && hours > 0 && minutes > 0) {
+            // Add participant's name to colpanel1
+            JLabel nameLabel = new JLabel(firstName + " " + lastName);
+            colpanel1.add(nameLabel);
+
+            // Display thank you message
+            JLabel messageLabel = new JLabel("Thank you, you were added as a participant.");
+            colpanel1.add(messageLabel);
+
+            // Refresh the panel to show the new participant and message
+            colpanel1.revalidate();
+            colpanel1.repaint();
+        }
     }
 }
-
-
